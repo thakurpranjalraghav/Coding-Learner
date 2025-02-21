4f0e31b91e3934835b40aca378be3a1625671917
Pranjal Raghav <125109625+thakurpranjalraghav@users.noreply.github.com>
2025-02-21 22:51:03 +0530

    Create Remove_Linked_List.py
    Day 12  21-02-2025

---
+++ Remove_Linked_List.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head  # Base case: return the last node as the new head
+        
+        new_head = self.reverseList(head.next)  # Reverse the rest of the list
+        head.next.next = head  # Make next node point to current node
+        head.next = None  # Break old link
+        
+        return new_head  # Return new head of the reversed list
