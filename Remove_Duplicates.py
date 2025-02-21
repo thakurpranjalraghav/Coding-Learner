0778cd0903be0c1b254e953df4281118233676be
Pranjal Raghav <125109625+thakurpranjalraghav@users.noreply.github.com>
2025-02-21 22:43:47 +0530

    Create Remove_Duplicates.py
    Day 13 21-02-2025

---
+++ Remove_Duplicates.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head  # Pointer to traverse the linked list
+        
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next  # Skip the duplicate node
+            else:
+                current = current.next  # Move to the next node
+        
+        return head  # Return the modified linked list
